c08047d494ce91d5ff5418d26b6cff8c2b01e57a
Andrés Ospina <adrosscosmic@gmail.com>
2026-04-30 22:14:43 -0500

    Se corrige error visual

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -10,7 +10,7 @@ const texts = TEXTS.views.about
   <main class="container about">
     <section class="u-flex u-row u-w-100 u-gap-xl">
       <div class="u-flex u-w-50" style="">
-        <h1>{{ texts.title }}</h1>
+        <h1 class="title u-mb-l">{{ texts.title }}</h1>
         <div class="u-flex" v-html="texts.description"></div>
       </div>
       <div class="u-flex u-w-50 u-justify-start u-align-start">
